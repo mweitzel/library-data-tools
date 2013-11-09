1c32fc98129a49441b568a54f69aec38843a4323
Matthew Weitzel <mweitzel@gmail.com>
2013-11-09 11:37:40 -0500

    rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibraryDataTools::Application.config.secret_key_base = 'a3380a8406771eb0a8e61cbcdd576a4ceb272b33873dea4479b4deed223b277de1888ca1945e5d95e0b664b8191978f216698228ce4900e3670d366890069294'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LibraryDataTools::Application.config.session_store :cookie_store, key: '_library-data-tools_session'
